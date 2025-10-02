75b2da3059fbc0f99fc6323470ff2504a08393b0
rizkysae <saifhafidz13@gmail.com>
2025-10-02 20:45:04 +0700

    a

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!");
+
+function addTodo() {
+    const todoInput = document.getElementById("todoInput");
+    const todoDate = document.getElementById("todoDate");
+    const todoList = document.getElementById("todoList");
+    console.log(todoInput.value, todoDate.value);
+    }
+
+    function deleteAllTodos() {
+    
+    }
+
+    function filterTodos() {
+    
+    }
--- README.md
+++
@@ -1,2 +0,0 @@
-# CodingCamp-290925-rizkysae
-Mini Project 29-09-2025
